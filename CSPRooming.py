43236e0e5ba4b9d926d58037949c12abbfc7c42d
HannahHorowitz <40675833+HannahHorowitz@users.noreply.github.com>
2019-04-29 19:46:42 -0500

    added main
    added main function to csp rooming

--- CSPRooming.py
+++ CSPRooming.py
@@ -1,4 +1,6 @@
 from CSP import *
+
+
 def readFromFile(filename):
     board = ""
     file = open(filename, "r")
@@ -7,3 +9,34 @@ def readFromFile(filename):
         board =line.replace("\n", "")
     return board
 
+
+def main():
+
+    # set up the variables and domains in the Australia example so it can be solved with AC3
+    variables = ["Northern Territory", "Queensland", "New South Wales", "Victoria"]
+    domains = {}
+    for variable in variables:
+        domains[variable] = ["red", "green", "blue"]
+
+    variables.append("South Australia")
+    domains["South Australia"] = ["red"]
+    variables.append("Western Australia")
+    domains["Western Australia"] = ["blue"]
+    variables.append("Tasmania")
+    domains["Tasmania"] = ["red"]
+
+    myCSP = CSP(variables, domains)
+    myCSP.addConstraint("Western Australia", "Northern Territory")
+    myCSP.addConstraint("Western Australia", "South Australia")
+    myCSP.addConstraint("South Australia", "Northern Territory")
+    myCSP.addConstraint("Queensland", "Northern Territory")
+    myCSP.addConstraint("Queensland", "South Australia")
+    myCSP.addConstraint("Queensland", "New South Wales")
+    myCSP.addConstraint("New South Wales", "South Australia")
+    myCSP.addConstraint("Victoria", "South Australia")
+    myCSP.addConstraint("Victoria", "New South Wales")
+
+    myCSP.ac3()
+    myCSP.print()
+
+main()
