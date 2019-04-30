23219d8c512fd16e6e176710a9ccc0d7250cf850
HannahHorowitz <40675833+HannahHorowitz@users.noreply.github.com>
2019-04-29 19:28:03 -0500

    added starting files
    Added CSP and CSPRooming based off of united states example

---
+++ CSPRooming.py
@@ -0,0 +1,9 @@
+from CSP import *
+def readFromFile(filename):
+    board = ""
+    file = open(filename, "r")
+    lines = file.readlines()
+    for line in lines:
+        board =line.replace("\n", "")
+    return board
+
