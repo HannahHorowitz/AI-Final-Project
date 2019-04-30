6110d2dc73a6a3a7e8547453cfdfbcd9b8b8aabd
HannahHorowitz <40675833+HannahHorowitz@users.noreply.github.com>
2019-04-29 20:45:19 -0500

    added outline of stuff
    added outline of forloop to separate strings and add domains and restraints

--- CSPRooming.py
+++ CSPRooming.py
@@ -11,6 +11,20 @@ def readFromFile(filename):
 
 
 def main():
+    lines = readFromFile("preferences.txt") #reading in text file of preferences
+
+    variables =[] #list of all variables, in this case the names entered
+    domains ={}
+    for line in lines: #foor loop to separate elements of the string in lines
+        #figure out how to separate the inputs based on comma and tab - separate into strings
+        #variables to save values into
+        # name
+        # roommate
+        # morning
+        # messy
+        # conflict
+        variables.append(name)
+        domains[name] =
 
     # set up the variables and domains in the Australia example so it can be solved with AC3
     variables = ["Northern Territory", "Queensland", "New South Wales", "Victoria"]
