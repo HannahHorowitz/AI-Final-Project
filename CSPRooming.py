6f2b98e3feeb3a67ee22a7863f60c70aa5e0662e
Josie Bauer <josie.bauer@drake.edu>
2019-05-06 18:30:46 -0500

    changed for loops

--- CSPRooming.py
+++ CSPRooming.py
@@ -22,51 +22,46 @@ def main():
     index = 0
     for x in lines:
         row = x.split()
-        var1 = row[0]
-        var2 = row[1]
-        var3 = row[2]
-        var4 = row[3]
-        name.append(var1)
-        pref.append(var2)
-        morning.append(var3)
-        messy.append(var4)
+        names = row[0]
+        preferences = row[1]
+        timeOfDay = row[2]
+        cleanliness = row[3]
+        name.append(names)
+        pref.append(preferences)
+        morning.append(timeOfDay)
+        messy.append(cleanliness)
         #print(name[index])
         index += 1
+
     for a in morning:
-        if a == "Morning" or a == "Night":
+        if a == "Morning":
             m = a.index("Morning")
-            n = a.index("Night")
             for b in name:
-                if b == name[m] or b == name[n]:
+                if b == name[m]:
                     print(name[m])
-                    print(name[n])
-    for c in morning:
-        if c == "Night":
-            n = c.index("Night")
-            for d in name:
-                if d == name[n]:
+        else:
+            n = a.index("Night")
+            for c in name:
+                if c == name[n]:
                     print(name[n])
 
-    for e in messy:
-        if e == "Messy":
-            m2 = e.index("Messy")
-            for f in name:
-                if f == name[m2]:
+    for d in messy:
+        if d == "Messy":
+            m2 = d.index("Messy")
+            for e in name:
+                if e == name[m2]:
                     print(name[m2])
-
-    for g in messy:
-        if g == "Neat":
-            n2 = g.index("Neat")
-            for h in name:
-                if h == name[n2]:
+        else:
+            n2 = d.index("Neat")
+            for f in name:
+                if f == name[n2]:
                     print(name[n2])
 
+    print(name)
+    print(pref)
+    print(morning)
+    print(messy)
 
-
-    #print(name)
-    #print(pref)
-    #print(morning)
-    #print(messy)
 
 
     variables =[] #list of all variables, in this case the names entered
